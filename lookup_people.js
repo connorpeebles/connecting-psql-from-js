d2008a9c18ce827e7301c292b74e64a955731a9c
Connor Peebles <connorpeebles93@gmail.com>
2018-10-16 14:52:06 +0000

    wrote the callback function printPeopleByFirstName which is called by the main function

--- lookup_people.js
+++ lookup_people.js
@@ -14,11 +14,16 @@ const printPeopleByFirstName = (name, arr) => {
   console.log("Searching...");
   console.log(`Found ${arr.length} person(s) by the name '${name}':`);
 
-  // for (let i = 0; i < result.rows.length; i++) {
-  //   console.log();
-  // }
+  for (let i = 0; i < arr.length; i++) {
+    const year = String(arr[i].birthdate.getFullYear());
+    let month = String(arr[i].birthdate.getMonth() + 1);
+    if (month.length === 1) month = "0" + month;
+    let day = String(arr[i].birthdate.getDate());
+    if (day.length === 1) day = "0" + day;
+    const date = year + "-" + month + "-" + day;
 
-  console.log(arr);
+    console.log(`- ${i+1}: ${arr[i].first_name} ${arr[i].last_name}, born '${date}'`);
+  }
 };
 
 function selectPeopleByFirstName(name, callback) {
